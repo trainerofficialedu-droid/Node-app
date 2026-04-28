37f6fbdbd701edaf81fb9df8fce37bb83d682fec
trainerofficialedu-droid <trainer.official.edu@gmail.com>
2026-04-28 13:01:37 +0530

    Update DeploymentTimeline.js

--- frontend/src/components/DeploymentTimeline.js
+++ frontend/src/components/DeploymentTimeline.js
@@ -3,7 +3,7 @@ import React from 'react';
 function DeploymentTimeline({ deployments }) {
   return (
     <div className="card glass">
-      <h2>Deployment Timeline</h2>
+      <h2>Deployment Timeline </h2>
       {deployments.map(item => (
         <p key={item.id}> {item.message} — {item.time}</p>
       ))}
