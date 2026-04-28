b30e11f115d5128e0b8382271d4160dcf8ca4dde
trainerofficialedu-droid <trainer.official.edu@gmail.com>
2026-04-28 13:25:02 +0530

    Update MetricsPanel.js another checiking

--- frontend/src/components/MetricsPanel.js
+++ frontend/src/components/MetricsPanel.js
@@ -3,7 +3,7 @@ import React from 'react';
 function MetricsPanel({ metrics }) {
   return (
     <div className="card glass">
-      <h2>Live Server Metrics </h2>
+      <h2>Live Server for Metrics </h2>
       <p><strong>CPU Usage:</Strong> {metrics.cpu}%</p>
       <p><strong>Memory Usage:</strong> {metrics.memory}%</p>
       <p><strong>Visitors Today:</strong> {metrics.visitors}</p>
