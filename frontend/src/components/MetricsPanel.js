adc4297f15773852ecbcfaa7cfa07e42aadc2885
trainerofficialedu-droid <trainer.official.edu@gmail.com>
2026-04-28 13:24:00 +0530

    Update MetricsPanel.js for checking

--- frontend/src/components/MetricsPanel.js
+++ frontend/src/components/MetricsPanel.js
@@ -4,7 +4,7 @@ function MetricsPanel({ metrics }) {
   return (
     <div className="card glass">
       <h2>Live Server Metrics </h2>
-      <p><strong>CPU Usage:</strong> {metrics.cpu}%</p>
+      <p><strong>CPU Usage:</Strong> {metrics.cpu}%</p>
       <p><strong>Memory Usage:</strong> {metrics.memory}%</p>
       <p><strong>Visitors Today:</strong> {metrics.visitors}</p>
       <p><strong>Total Deployments:</strong> {metrics.deployments}</p>
